FIX: (model_lgbm_baseline) - fixed feature generation and testing (on first 100k lines as head) - not implemented shift during rolling - still miss long-term moving averages

kernels/DecisionTree/test_model_lgbm_baseline.py
@@ -1,6 +1,7 @@
 import unittest
 import model_lgbm_baseline
 import pandas as pd
+import numpy as np
 
 
 class testcase(unittest.TestCase):
@@ -40,15 +41,28 @@ class testcase(unittest.TestCase):
         self.assertFalse(complete_features.empty)
         self.assertTrue('weekday' in complete_features.columns)
 
-        poss = [9000, 9200, 9500]
+        # this check takes some lagged features and
+        # manually compute expected lagged values
+        # to check the values are correct
+
+        poss = [90000, 92000, 95000]
         cols = ['returnsClosePrevRaw10', 'returnsOpenPrevMktres10']
         lags = ['3', '7']
         for pos in poss:
             for lag in lags:
                 for col in cols:
+                    #import pdb;pdb.set_trace()
+                    # value computed in feature generation
                     got = complete_features.iloc[pos]['lag_'+lag+'_'+col+'_max']
+                    # code of the asset that on which are checking lagged feat
                     code = self.market_train_df.iloc[pos]['assetCode']
-                    real = self.market_train_df[self.market_train_df['assetCode'] == code][col][-(int(lag)):].fillna(0).max()
+                    # this is the time_serie of the asset 'code'
+                    time_serie = self.market_train_df[self.market_train_df['assetCode'] == code][col].reset_index()
+                    # we are checking the i-th day 'check_day' of the time_serie
+                    check_day = np.where(time_serie['index'] == pos)[0][0]
+                    # time window
+                    time_window = time_serie.iloc[check_day-int(lag)+1:check_day+1,1]
+                    real = time_window.fillna(0).max()
                     self.assertEqual(got, real)
 
         print("generate features test OK")